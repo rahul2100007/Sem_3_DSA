29b7a0fa4349bd76e05d97244bf385270f88501c
Rahul Prajapati <rahulprajapati23@gnu.ac.in>
2024-07-28 00:05:47 +0530

    add line

--- PRAC1_1.c
+++ PRAC1_1.c
@@ -2,6 +2,7 @@
 
 int main(){
     float salary, GS, FS;
+    printf("Enter your salary: ");
     printf("Enter the salary of the employee: ");
     scanf("%f", &salary);
     GS = (salary / 100) * 60;
