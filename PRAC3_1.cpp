43c785f2345d3204bbd62262bf99d46886e15344
Rahul Prajapati <rahulprajapati23@gnu.ac.in>
2024-07-30 20:42:38 +0530

    add file

--- PRAC3_1.cpp
+++ PRAC3_1.cpp
@@ -1,71 +1,54 @@
 #include <iostream>
-#include <stack>
 #include <vector>
-#include <sstream>
-
 using namespace std;
-
-stack<int> i_ball;
-
-bool push_ball(int x) {
-    if (i_ball.size() >= 9) {
-        cout << "Cannot add more than 9 balls!" << endl;
-        return false;
+vector<int> i_ball;
+int ball_push()
+{
+    int ball;
+    cout << "enter ball number:";
+    cin >> ball;
+    i_ball.push_back(ball);
+};
+int pop_ball()
+{
+    if (i_ball.size() > 0)
+    {
+        cout << "ball poped" << endl;
+        i_ball.pop_back();
     }
-    i_ball.push(x);
-    return true;
-}
-
-void pop_ball() {
-    if (i_ball.empty()) {
-        cout << "No balls to delete!" << endl;
-        return;
+};
+void display(vector<int> &vec)
+{
+    int size = vec.size();
+    cout<<"total balls are in your basket is:\t";
+    cout<<"[";
+    for (int i = 0; i < size; i++)
+    {
+        cout << vec[i] << " ";
     }
-    i_ball.pop();
+    cout << "]" << endl;
 }
-
-void displayBalls() {
-    stack<int> temp = i_ball;
-    vector<int> balls;
+int main()
+{
+    int choice;
     
-    // Transfer elements to a vector to display them
-    while (!temp.empty()) {
-        balls.push_back(temp.top());
-        temp.pop();
+    for (int k = 0; k < 9; k++)
+    {
+            cout << "1.push\n2.pop\n0.to end game and display score\n";
+            cin >> choice;
+            switch (choice)
+            {
+            case 1:
+                ball_push();
+                break;
+            case 2:
+                pop_ball();
+                break;
+            }
+            if(choice==0){
+                display(i_ball);
+                cout << "\nexiting...";
+                k=9;
+            }
     }
-    
-    // Display elements in reverse order to match stack order
-    for (auto it = balls.rbegin(); it != balls.rend(); ++it) {
-        cout << *it << " ";
-    }
-    cout << endl;
-}
-
-int main() {
-    string input;
-    cout << "Enter the sequence of ball numbers and operations (e.g., '1 8 9 7 2 3'): ";
-    getline(cin, input);
-
-    stringstream ss(input);
-    vector<int> numbers;
-    int num;
-
-    while (ss >> num) {
-        numbers.push_back(num);
-    }
-
-    // Simulate the operations
-    for (int i = 0; i < numbers.size(); ++i) {
-        if (i == 4 || i == 5) { // Pop operations at 5th and 6th positions
-            pop_ball();
-        } else {
-            push_ball(numbers[i]);
-        }
-    }
-
-    // Display the final state of the basket
-    cout << "Final state of the basket: ";
-    displayBalls();
-
-    return 0;
 }
